fb2819f84ee66ec9b389d8a7026c0020c551268b
anas zabili <anaszab78@gmail.com>
2021-02-13 23:31:24 +0100

    lesson 75 (ref en reactive SUPER IMPORTANT)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="home">
-    Home
-    <p>my name is {{ name }}</p>
-    <p>my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    let name = "mario";
-    let age = "30";
-
-    const handleClick = () => console.log("you click me");
-    return {
-      name: name,
-      age: age,
-      handleClick,
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    // avantage de reactive on a plus besoin de preciser .value
+    // desavantage nous ne pouvons pas traiter les types primitve
+    // donc de façon general ref est tres largement privilegié
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
     };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
+    };
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
   },
 };
 </script>
