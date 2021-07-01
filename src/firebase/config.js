153a845e3eb048d168c21e6b9d280684e7faa49b
anasZabili <57200540+anasZabili@users.noreply.github.com>
2021-07-02 01:04:29 +0200

    Fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8LzarLqGpTHuKb_F-7-JxEUefG9se80U",
-  authDomain: "udemy-vue-tnn-blog.firebaseapp.com",
-  projectId: "udemy-vue-tnn-blog",
-  storageBucket: "udemy-vue-tnn-blog.appspot.com",
-  messagingSenderId: "285478383757",
-  appId: "1:285478383757:web:0d272a4a28298c39d932c4",
+  x: "xxx"
 };
 
 // init firebase
